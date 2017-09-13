5ccdaaf8fb943c824979776368a0373944463756
paresharma <paresh.brahm@gmail.com>
2017-09-13 12:52:02 +0900

    Add assets as an engine initializer

--- lib/flagstrap.rb
+++ lib/flagstrap.rb
@@ -2,5 +2,10 @@ require "flagstrap/version"
 
 module Flagstrap
   class Engine < ::Rails::Engine
+    initializer 'flagstrap.assets' do |app|
+      %w(css js).each do |sub|
+        app.config.assets.paths << root.join('dist', sub).to_s
+      end
+    end
   end
 end
--- lib/flagstrap/version.rb
+++ lib/flagstrap/version.rb
@@ -1,3 +1,3 @@
 module Flagstrap
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
